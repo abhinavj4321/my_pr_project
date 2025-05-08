0fd96cac3201089d905cc5c06f6c4b6e7fccf820
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-05-08 21:37:58 +0530

    Removed chatbot folder completely

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('query/', views.chatbot_query, name="chatbot_query"),
-    path('feedback/', views.chatbot_feedback, name="chatbot_feedback"),
-]
