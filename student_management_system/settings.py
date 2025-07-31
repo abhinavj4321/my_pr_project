573880d8a4aaff9004774c6b46f8e1aa9e201ac0
Abhinav Jaiswal <abhinavj4321@gmail.com>
2025-07-31 14:35:52 +0530

    Fix PostgreSQL compatibility: Update to psycopg3 and correct Django WSGI start command

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -66,11 +66,15 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+import dj_database_url
+
+# Database configuration with PostgreSQL for production
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
